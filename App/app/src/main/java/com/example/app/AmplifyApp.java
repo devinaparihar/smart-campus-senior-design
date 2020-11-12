d2d242c20711f96eca181a210c78af291be2d490
sDauenbaugh <s.dauenbaugh@gmail.com>
2020-11-11 18:44:56 -0600

    added GraphQL api

--- App/app/src/main/java/com/example/app/AmplifyApp.java
+++ App/app/src/main/java/com/example/app/AmplifyApp.java
@@ -4,7 +4,10 @@ import android.app.Application;
 import android.util.Log;
 
 import com.amplifyframework.AmplifyException;
+import com.amplifyframework.api.aws.AWSApiPlugin;
+import com.amplifyframework.api.graphql.model.ModelMutation;
 import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.generated.model.Todo;
 
 public class AmplifyApp extends Application {
 
@@ -12,10 +15,23 @@ public class AmplifyApp extends Application {
         super.onCreate();
 
         try {
+            Amplify.addPlugin(new AWSApiPlugin());
             Amplify.configure(getApplicationContext());
+
             Log.i("MyAmplifyApp", "Initialized Amplify");
         } catch (AmplifyException error) {
             Log.e("MyAmplifyApp", "Could not initialize Amplify", error);
         }
+
+        Todo todo = Todo.builder()
+                .name("My first todo")
+                .description("todo description")
+                .build();
+
+        Amplify.API.mutate(
+                ModelMutation.create(todo),
+                response -> Log.i("MyAmplifyApp", "Added Todo with id: " + response.getData().getId()),
+                error -> Log.e("MyAmplifyApp", "Create failed", error)
+        );
     }
 }
