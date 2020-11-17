84937dc4afdd85bd75d5d8cc46cb805416d08694
thomasw219 <thomasw219@gmail.com>
2020-11-17 17:04:16 -0600

    gaussian mixture model implemented, of variable number of components as well, also variable number of hidden layers, works w/ example circle problem

--- gmn.py
+++ gmn.py
@@ -6,13 +6,10 @@ import torch.optim as optim
 import torch.distributions as distributions
 
 INPUT_SIZE = 2
-HIDDEN_STATE_SIZE = 256
 OUTPUT_SIZE = 5
 
 GRAD_CLIP = 10
 LOSS_GRAD_CLIP = 100
-BATCH_SIZE = 64
-LEARNING_RATE = 0.001
 
 CUDA = False #cuda.is_available()
 
@@ -28,6 +25,21 @@ def y_to_params(y):
     means = torch.cat((mu_1, mu_2), dim=2)
     cov_matrices = torch.cat((sigma_1 * sigma_1, cov, cov, sigma_2 * sigma_2), dim=2).view(y.size()[0], y.size()[1], 2, 2)
     return means, cov_matrices
+
+def params_to_mixture_model(g_params, weights, components):
+    means_list = []
+    cov_list = []
+    for i in range(components):
+        y_i = g_params.narrow(2, i * OUTPUT_SIZE, OUTPUT_SIZE)
+        mean, cov = y_to_params(y_i)
+        means_list.append(mean)
+        cov_list.append(cov)
+    means = torch.stack(means_list, dim=2)
+    covariance_matrices = torch.stack(cov_list, dim=2)
+    dist = distributions.multivariate_normal.MultivariateNormal(means, covariance_matrix=covariance_matrices)
+    categorical = distributions.categorical.Categorical(probs=weights)
+    mixture = distributions.mixture_same_family.MixtureSameFamily(categorical, dist)
+    return mixture, means
 
 def numpy_data_to_pytorch(l_in):
     max_len = max([x.shape[0] for x in l_in])
@@ -56,89 +68,101 @@ def sample(data_l, mask_l, size):
 
 class DeepLSTM(nn.Module):
 
-    def __init__(self):
+    def __init__(self, n_hidden_layers, hidden_state_size, n_mixture_components):
         super(DeepLSTM, self).__init__()
-        self.lstm1 = nn.LSTMCell(INPUT_SIZE, HIDDEN_STATE_SIZE)
-        self.r1 = nn.ReLU()
-        self.lstm2 = nn.LSTMCell(INPUT_SIZE + HIDDEN_STATE_SIZE, HIDDEN_STATE_SIZE)
-        self.r2 = nn.ReLU()
-        self.l1 = nn.Linear(INPUT_SIZE + HIDDEN_STATE_SIZE, OUTPUT_SIZE)
+        self.n_hidden_layers = n_hidden_layers
+        self.n_mixture_components = n_mixture_components
+        self.hidden_state_size = hidden_state_size
+        layers_list = []
+        layers_list.append(nn.LSTMCell(INPUT_SIZE, hidden_state_size))
+        for i in range(n_hidden_layers - 1):
+            layers_list.append(nn.LSTMCell(INPUT_SIZE + hidden_state_size, hidden_state_size))
+        self.layers = nn.ModuleList(layers_list)
+        self.gaussian_params_linear = nn.Linear(INPUT_SIZE + hidden_state_size, OUTPUT_SIZE * n_mixture_components)
+        self.weights_linear = nn.Linear(INPUT_SIZE + hidden_state_size, n_mixture_components)
+        self.softmax = nn.Softmax()
 
-    def step(self, x_t, h_1_tm1, c_1_tm1, h_2_tm1, c_2_tm1):
+    def step(self, x_t, h_tm1, c_tm1):
             x_1_t = x_t
-            h_1_t, c_1_t = self.lstm1(x_1_t, (h_1_tm1, c_1_tm1))
+            h_im1_t, c_im1_t = self.layers[0](x_1_t, (h_tm1[0], c_tm1[0]))
 
-            if h_1_t.requires_grad:
-                h_1_t.register_hook(lambda x: x.clamp(min=-1 * GRAD_CLIP, max=GRAD_CLIP))
-            if c_1_t.requires_grad:
-                c_1_t.register_hook(lambda x: x.clamp(min=-1 * GRAD_CLIP, max=GRAD_CLIP))
+            if h_im1_t.requires_grad:
+                h_im1_t.register_hook(lambda x: x.clamp(min=-1 * GRAD_CLIP, max=GRAD_CLIP))
+            if c_im1_t.requires_grad:
+                c_im1_t.register_hook(lambda x: x.clamp(min=-1 * GRAD_CLIP, max=GRAD_CLIP))
 
-            x_2_t = torch.cat((x_t, h_1_t), dim=1)
-            h_2_t, c_2_t = self.lstm2(x_2_t, (h_2_tm1, c_2_tm1))
+            h_t = [h_im1_t]
+            c_t = [c_im1_t]
+            for i in range(self.n_hidden_layers - 1):
+                x_i_t = torch.cat((x_t, h_im1_t), dim=1)
+                h_i_t, c_i_t = self.layers[i + 1](x_i_t, (h_tm1[i + 1], c_tm1[i + 1]))
+                h_t.append(h_i_t)
+                c_t.append(c_i_t)
+                if h_i_t.requires_grad:
+                    h_i_t.register_hook(lambda x: x.clamp(min=-1 * GRAD_CLIP, max=GRAD_CLIP))
+                if c_i_t.requires_grad:
+                    c_i_t.register_hook(lambda x: x.clamp(min=-1 * GRAD_CLIP, max=GRAD_CLIP))
+                h_im1_t = h_i_t
+                c_im1_t = c_i_t
 
-            if h_2_t.requires_grad:
-                h_2_t.register_hook(lambda x: x.clamp(min=-1 * GRAD_CLIP, max=GRAD_CLIP))
-            if c_2_t.requires_grad:
-                c_2_t.register_hook(lambda x: x.clamp(min=-1 * GRAD_CLIP, max=GRAD_CLIP))
+            x_n_t = torch.cat((x_t, h_im1_t), dim=1)
+            g_params = self.gaussian_params_linear(x_n_t)
+            weights = self.softmax(self.weights_linear(x_n_t))
 
-            x_3_t = torch.cat((x_t, h_2_t), dim=1)
-            y_t = self.l1(x_3_t)
+            return g_params, weights, h_t, c_t
 
-            return y_t, h_1_t, c_1_t, h_2_t, c_2_t
-
-    def init_hidden(self):
-        h_1 = torch.zeros((x.size()[0], HIDDEN_STATE_SIZE))
-        c_1 = torch.zeros((x.size()[0], HIDDEN_STATE_SIZE))
-        h_2 = torch.zeros((x.size()[0], HIDDEN_STATE_SIZE))
-        c_2 = torch.zeros((x.size()[0], HIDDEN_STATE_SIZE))
-        if CUDA:
-            h_1 = h_1.cuda()
-            c_1 = c_1.cuda()
-            h_2 = h_2.cuda()
-            c_2 = c_2.cuda()
-
-        return h_1, c_1, h_2, c_2
+    def init_hidden(self, x):
+        h_0 = []
+        c_0 = []
+        for i in range(self.n_hidden_layers):
+            h_i_0 = torch.zeros((x.size()[0], self.hidden_state_size))
+            c_i_0 = torch.zeros((x.size()[0], self.hidden_state_size))
+            if CUDA:
+                h_i_0 = h_i_0.cuda()
+                c_i_0 = c_i_0.cuda()
+            h_0.append(h_i_0)
+            c_0.append(c_i_0)
+        return h_0, c_0
 
     def forward(self, x):
-        h_1_tm1, c_1_tm1, h_2_tm1, c_2_tm1 = self.init_hidden()
-        y_list = []
+        h_tm1, c_tm1 = self.init_hidden(x)
+        g_param_list = []
+        weights_list = []
 
         for i in range(x.size()[1]):
             x_t = torch.squeeze(x.narrow(1, i, 1), 1)
-            y_t, h_1_tm1, c_1_tm1, h_2_tm1, c_2_tm1 = self.step(x_t, h_1_tm1, c_1_tm1, h_2_tm1, c_2_tm1)
-            y_list.append(y_t)
+            g_params_t, weights_t, h_tm1, c_tm1 = self.step(x_t, h_tm1, c_tm1)
+            g_param_list.append(g_params_t)
+            weights_list.append(weights_t)
 
-        return torch.stack(y_list, dim=1)
+        return torch.stack(g_param_list, dim=1), torch.stack(weights_list, dim=1)
 
     def generate(self, x, steps):
-        h_1_tm1, c_1_tm1, h_2_tm1, c_2_tm1 = self.init_hidden()
-        y_tm1 = None
+        h_tm1, c_tm1 = self.init_hidden(x)
+        g_params_tm1 = None
+        weights_tm1 = None
 
         for i in range(x.size()[1]):
             x_t = torch.squeeze(x.narrow(1, i, 1), 1)
-            y_tm1, h_1_tm1, c_1_tm1, h_2_tm1, c_2_tm1 = self.step(x_t, h_1_tm1, c_1_tm1, h_2_tm1, c_2_tm1)
-            means, cov_matrices = y_to_params(torch.stack([y_tm1], dim=1))
+            g_params_tm1, weights_tm1, h_tm1, c_tm1 = self.step(x_t, h_tm1, c_tm1)
 
         new_points = []
         for i in range(steps):
-            means, cov_matrices = y_to_params(torch.stack([y_tm1], dim=1))
-            print("Mean: {}".format(means))
-            print("Covariance: {}".format(means))
-#            dist = distributions.multivariate_normal.MultivariateNormal(means, covariance_matrix=cov_matrices)
-#            x_t = dist.sample().squeeze(dim=1)
-            x_t = means.squeeze(dim=0)
+            stacked_g_params = torch.stack([g_params_tm1], dim=1)
+            stacked_weights = torch.stack([weights_tm1], dim=1)
+            dist, _ = params_to_mixture_model(stacked_g_params, stacked_weights, self.n_mixture_components)
+            x_t = dist.mean.squeeze(dim=0)
             new_points.append(x_t)
-            y_tm1, h_1_tm1, c_1_tm1, h_2_tm1, c_2_tm1 = self.step(x_t, h_1_tm1, c_1_tm1, h_2_tm1, c_2_tm1)
+            g_params_tm1, weights_tm1, h_tm1, c_tm1 = self.step(x_t, h_tm1, c_tm1)
 
         return torch.stack(new_points, dim=1)
 
-def forward_pass_batch(x, mask):
+def forward_pass_batch(net, x, mask, components):
     x_train = x.narrow(1, 0, x.size()[1] - 1)
     y_train = x.narrow(1, 1, x.size()[1] - 1)
     mask_train = mask.narrow(1, 1, mask.size()[1] - 1)
-    output = net.forward(x_train)
-    means, cov_matrices = y_to_params(output)
-    dist = distributions.multivariate_normal.MultivariateNormal(means, covariance_matrix=cov_matrices)
+    g_params, weights = net.forward(x_train)
+    dist, means = params_to_mixture_model(g_params, weights, components)
     loss = -1 * torch.sum(dist.log_prob(y_train) * mask_train) / y_train.size()[0]
     if loss.requires_grad:
         loss.register_hook(lambda x: x.clamp(min=-1 * LOSS_GRAD_CLIP, max=LOSS_GRAD_CLIP))
--- train_circles.py
+++ train_circles.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 from gmn import *
 
-DIR = "./images2/"
+DIR = "./images_mixtureof2/"
 def save_as_image(input_points, generated_points, name):
     plt.figure()
     np_input = input_points.cpu().numpy()[0]
@@ -32,9 +32,16 @@ min_loss = np.inf
 min_means = None
 min_y = None
 min_mask = None
+
+BATCH_SIZE = 2
+LEARNING_RATE = 0.001
+HIDDEN_STATE_SIZE = 256
+N_LAYERS = 2
+N_COMPONENTS = 2
+
 for state in range(1):
     print("Model: {}".format(state))
-    net = DeepLSTM()
+    net = DeepLSTM(N_LAYERS, HIDDEN_STATE_SIZE, N_COMPONENTS)
     if CUDA:
         net = net.cuda()
     optimizer = optim.Adam(net.parameters(), lr=LEARNING_RATE)
@@ -43,22 +50,23 @@ for state in range(1):
         print("Epoch: {}".format(i))
         optimizer.zero_grad()
         x, mask = sample(train_data, train_mask, BATCH_SIZE)
-        train_loss, _, _, _ = forward_pass_batch(x, mask)
+        train_loss, _, _, _ = forward_pass_batch(net, x, mask, N_COMPONENTS)
         train_loss.backward()
         optimizer.step()
         print("Training Loss: {}".format(train_loss.item()))
         with torch.no_grad():
             x, mask = sample(test_data, test_mask, BATCH_SIZE)
-            test_loss, means, y_test, mask_test = forward_pass_batch(x, mask)
+            test_loss, means, y_test, mask_test = forward_pass_batch(net, x, mask, N_COMPONENTS)
             print("Test Loss: {}".format(test_loss.item()))
             if test_loss.item() < min_loss:
                 min_loss = test_loss.cpu().item()
                 x, mask = sample(val_data, val_mask, 1)
-                loss, means, y_test, mask_test = forward_pass_batch(x, mask)
+                loss, means, y_test, mask_test = forward_pass_batch(net, x, mask, N_COMPONENTS)
                 min_means = means
                 min_y = y_test
                 min_mask = mask_test
                 np_mask = mask.cpu().numpy()
+
                 n = max([i + 1 if np_mask[0, i] == 1 else 0 for i in range(np_mask.shape[1])])
                 k = np.random.randint(3, n - 6)
                 x_cut = x.narrow(1, 0, n - k)
@@ -68,12 +76,13 @@ for state in range(1):
                 print("Generated Output:\n{}".format(x_gen))
                 print("True Output:\n{}".format(x.narrow(1, n - k, k)))
                 print("FORWARD PASS FOR REFERENCE")
-                loss, means, y, mask = forward_pass_batch(x, mask)
+                loss, means, y, mask = forward_pass_batch(net, x, mask, N_COMPONENTS)
                 print("Means:\n{}".format(means))
                 print("Target:\n{}".format(y))
                 save_as_image(x_cut, x_gen, "Epoch_{:04d}_TestLoss_{}".format(i, test_loss.cpu().item()))
 
         print("Min test loss: {}".format(min_loss))
-        print("Min test loss difference:\n{}".format(torch.abs(min_means - min_y)))
+        #print("Min test loss difference:\n{}".format(torch.abs(min_means - min_y)))
+        print("Min test loss means:\n{}".format(min_means))
         print("Min test loss target:\n{}".format(min_y))
         print("Min test loss mask:\n{}".format(min_mask))
