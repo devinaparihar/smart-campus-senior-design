a9e990ba64945381a75456a0c6df2616ffc44ab5
thomasw219 <thomasw219@gmail.com>
2021-04-08 03:13:32 -0500

    add separate time prediction LSTM

--- wlan_data/discrete_lstm.py
+++ wlan_data/discrete_lstm.py
@@ -12,17 +12,16 @@ LOSS_GRAD_CLIP = 100
 
 class DeepLSTM(nn.Module):
 
-    def __init__(self, n_hidden_layers, hidden_state_size, dim):
+    def __init__(self, n_hidden_layers, hidden_state_size, input_dim, output_dim):
         super(DeepLSTM, self).__init__()
         self.n_hidden_layers = n_hidden_layers
-        self.dim = dim
         self.hidden_state_size = hidden_state_size
         layers_list = []
-        layers_list.append(nn.LSTMCell(dim, hidden_state_size))
+        layers_list.append(nn.LSTMCell(input_dim, hidden_state_size))
         for i in range(n_hidden_layers - 1):
-            layers_list.append(nn.LSTMCell(dim + hidden_state_size, hidden_state_size))
+            layers_list.append(nn.LSTMCell(input_dim + hidden_state_size, hidden_state_size))
         self.layers = nn.ModuleList(layers_list)
-        self.linear = nn.Linear(dim + hidden_state_size, dim)
+        self.linear = nn.Linear(input_dim + hidden_state_size, output_dim)
 
     def step(self, x_t, h_tm1, c_tm1, final):
             x_1_t = x_t
@@ -80,11 +79,28 @@ class DeepLSTM(nn.Module):
         return outputs.index_select(1, torch.tensor([outputs.size()[1] - 1])).squeeze(dim=1)
 
 SOFTMAX = nn.Softmax(dim=-1)
-def forward_pass_batch(net, x, lossCE, num_CE, lossMSE, NUM_MSE):
+LOSS_CE = nn.CrossEntropyLoss()
+def location_forward_pass_batch(net, x, n_locations):
     x_train = x.narrow(1, 0, x.size()[1] - 1)
-    y_train = torch.argmax(x.narrow(1, 1, x.size()[1] - 1).narrow(2, 0, num_CE), dim=2)
-    probs = SOFTMAX(net.forward(x_train).narrow(2, 0, num_CE))
-    train_loss = lossCE(probs.permute(0, 2, 1), y_train)
+    y_train = torch.argmax(x.narrow(1, 1, x.size()[1] - 1).narrow(2, 0, n_locations), dim=2)
+    probs = SOFTMAX(net.forward(x_train))
+    train_loss = LOSS_CE(probs.permute(0, 2, 1), y_train)
+    if train_loss.requires_grad:
+        train_loss.register_hook(lambda x: x.clamp(min=-1 * LOSS_GRAD_CLIP, max=LOSS_GRAD_CLIP))
+    return train_loss
+
+LOSS_MSE = torch.nn.MSELoss()
+def time_forward_pass_batch(net, x, n_locations, train_length):
+    x_train = x.narrow(1, 0, train_length)
+    times = x.narrow(1, train_length - 1, 1).narrow(2, n_locations, 1).squeeze(dim=2)
+    samples = []
+    for y in times.numpy().ravel():
+        samples.append([np.random.uniform(high=y)])
+    samples = torch.tensor(samples)
+    y_train = times - samples
+    x_train[:, train_length - 1, n_locations] = samples[:, 0]
+    outputs = net.forward_next_step(x_train)
+    train_loss = LOSS_MSE(outputs, y_train)
     if train_loss.requires_grad:
         train_loss.register_hook(lambda x: x.clamp(min=-1 * LOSS_GRAD_CLIP, max=LOSS_GRAD_CLIP))
     return train_loss
@@ -108,7 +124,8 @@ one_hot_df, int_to_id, id_to_int = spaceid_to_one_hot(df_sorted_transitions)
 
 # get duration in space
 df_sorted_transitions = get_durations(df_sorted_transitions)
-df_sorted_transitions['DURATION_IN_SPACE_MINUTES'] = df_sorted_transitions['DURATION_IN_SPACE_SECONDS'].apply(lambda x: x/60 if type(x) is float else -1)
+# NEED TO DO SOMETHING ABOUT THESE NON FLOAT THINGS AT SOME POINT
+df_sorted_transitions['DURATION_IN_SPACE_MINUTES'] = df_sorted_transitions['DURATION_IN_SPACE_SECONDS'].apply(lambda x: x/60 if type(x) is float else 1)
 
 # append duration as feature to one hot encoded df
 one_hot_df['STAYING_TIME'] = df_sorted_transitions['DURATION_IN_SPACE_MINUTES'].tolist()
@@ -131,49 +148,87 @@ df = pd.read_csv("OneHot.csv")
 N_SPLITS = 50
 HIDDEN_STATE_SIZE = 100
 N_LAYERS = 3
-DIM = df.values.shape[1] - 2
-N_LOCATIONS = DIM - 1
-LR = 0.01
+N_LOCATIONS = df.values.shape[1] - 3
+INPUT_DIM = N_LOCATIONS + 1
+LOCATION_LR = 0.01
+TIME_LR = 0.005
 
 test_steps = [1, 2, 3, 4, 9]
-means = np.zeros((len(test_steps),))
+mean_accuracies = np.zeros((len(test_steps),))
+mean_errors = np.zeros((len(test_steps),))
 
 for split in range(N_SPLITS):
     print("Split: {}".format(split + 1))
     train_df, test_df = sequence_train_test_split(df, TRAIN_LENGTH)
 
-    network = DeepLSTM(N_LAYERS, HIDDEN_STATE_SIZE, DIM)
+    location_network = DeepLSTM(N_LAYERS, HIDDEN_STATE_SIZE, INPUT_DIM, N_LOCATIONS)
+    time_network = DeepLSTM(N_LAYERS, HIDDEN_STATE_SIZE, INPUT_DIM, 1)
     if CUDA:
-        network = network.cuda()
-    optimizer = optim.Adam(network.parameters(), lr=LR)
-    loss1 = nn.CrossEntropyLoss()
-    loss2 = torch.nn.MSELoss()
+        location_network = location_network.cuda()
+    location_optimizer = optim.Adam(location_network.parameters(), lr=LOCATION_LR)
+    time_optimizer = optim.Adam(time_network.parameters(), lr=TIME_LR)
 
     best_accuracies = [0 for i in range(len(test_steps))]
+    best_errors = [np.inf for i in range(len(test_steps))]
 
     for i in range(N_EPOCHS):
-        #print("Epoch: {}".format(i))
-        optimizer.zero_grad()
+        print("Epoch: {}".format(i))
+
+        # Train the location prediction network
+        location_optimizer.zero_grad()
         x = np.array(random_sample_data(train_df, TRAIN_LENGTH, N_PER_USER))
         x = torch.from_numpy(x).float()
-        train_loss_1 = forward_pass_batch(network, x, loss1, N_LOCATIONS, loss2, 1)
-        train_loss_1.backward()
-        #print("Training Loss: {}".format(train_loss_1.item()))
-        optimizer.step()
+        location_loss = location_forward_pass_batch(location_network, x, N_LOCATIONS)
+        location_loss.backward()
+        location_optimizer.step()
+        print("Location Loss: {}".format(location_loss.item()))
+
+        # Train the time prediction network
+        time_optimizer.zero_grad()
+        time_loss = torch.tensor(0).float()
+        for j, n in enumerate(test_steps):
+            x = np.array(random_sample_data(train_df, n + 1, TRAIN_LENGTH // (n + 1)))
+            x = torch.from_numpy(x).float()
+            time_loss += time_forward_pass_batch(time_network, x, N_LOCATIONS, n)
+        time_loss.backward()
+        time_optimizer.step()
+        print("Time Loss: {}".format(time_loss.item()), flush=True)
+
         with torch.no_grad():
+            # Evaluate test location accuracy
             for j, n in enumerate(test_steps):
                 data = np.stack(random_sample_data(test_df, n + 1, TRAIN_LENGTH // (n + 1)))
                 x = torch.from_numpy(data[:, :-1]).float()
                 y = np.argmax(data[:, -1, :-1], axis=1)
-                output = np.argmax(network.forward_next_step(x).numpy(), axis=1)
+                output = np.argmax(location_network.forward_next_step(x).numpy(), axis=1)
                 total = y.shape[0]
                 correct = np.sum(np.equal(y, output))
                 accuracy = correct / total
                 if best_accuracies[j] < accuracy:
                     best_accuracies[j] = accuracy
-                    #print("{}-step Accuracy: {} | {}/{}".format(n, accuracy, correct, total))
+                    print("{}-step Location Accuracy: {} | {}/{}".format(n, accuracy, correct, total))
+
+            # Evaluate test time error
+            for j, n in enumerate(test_steps):
+                data = np.stack(random_sample_data(test_df, n + 1, TRAIN_LENGTH // (n + 1)))
+                x = torch.from_numpy(data[:, :-1]).float()
+                times = x.narrow(1, n - 1, 1).narrow(2, N_LOCATIONS, 1).squeeze(dim=2)
+                samples = []
+                for y in times.numpy().ravel():
+                    samples.append([np.random.uniform(high=y)])
+                samples = torch.tensor(samples)
+                y = times - samples
+                x[:, n - 1, N_LOCATIONS] = samples[:, 0]
+                output = time_network.forward_next_step(x)
+                error = LOSS_MSE(output, y).item()
+                if best_errors[j] > error:
+                    best_errors[j] = error
+                    print("{}-step Test Error: {}".format(n, error))
+
     print("Best Accuracies: {}".format(best_accuracies))
     print("Test Steps: {}".format(test_steps))
 
-    means += np.array(best_accuracies)
-    print("Mean Accuracies: {}".format(means / (split + 1)))
+    mean_accuracies += np.array(best_accuracies)
+    mean_errors += np.array(best_errors)
+    print("Mean Accuracies: {}".format(mean_accuracies / (split + 1)))
+    print("Mean Errors: {}".format(mean_errors / (split + 1)))
